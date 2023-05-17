05b6b5b92f12364a2dea0ef9ae3e09e863aa6496
Luchs41 <kokobo6@gmail.com>
2023-05-17 19:20:33 +0900

    Dependencies added.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { Route, Routes, useNavigate, Router } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router></Router>
+    </div>
+  );
+}
+
+export default App;
