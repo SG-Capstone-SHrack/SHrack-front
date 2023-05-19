bd13fd84cdc26695888e66470e4999546a237a2e
Luchs41 <kokobo6@gmail.com>
2023-05-20 01:01:39 +0900

    validation 실패 alert 구현

--- src/account/Signin.jsx
+++ src/account/Signin.jsx
@@ -1,56 +1,81 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
-import { authAtom } from '../state';
-import { Button, Form } from 'react-bootstrap';
-import * as yup from 'yup';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+  Button,
+  Form,
+  Container,
+  FloatingLabel,
+  Alert,
+  Row,
+  Col,
+} from 'react-bootstrap';
+import axios from 'axios';
 
 function Signin() {
-  const validationSchema = yup.object().shape({
-    signInId: yup.string().required('아이디를 입력해주세요.'),
-    signInPassword: yup.string().required('비밀번호를 입력해주세요.'),
-  });
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [isAlert, setIsAlert] = useState(true);
+  useEffect(() => {
+    console.log(id, password);
+  }, [id, password]);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    mode: 'onBlur',
-    resolver: yupResolver(validationSchema),
-  });
-
-  const auth = useRecoilValue(authAtom);
-  const navigate = useNavigate();
-
-  const onSubmit = () => {
-    console.log('로그인');
+  // onSubmit -> Id와 Password를 받아서 서버에 전송
+  const onSubmit = data => {
+    console.log(data);
+    // 서버에 전송
   };
-
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="signInId">
-        <Form.Label>아이디</Form.Label>
-        <Form.Control type="ID" placeholder="아이디" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="signInPassword">
-        <Form.Label>비밀번호</Form.Label>
-        <Form.Control type="password" placeholder="비밀번호" />
-      </Form.Group>
-
-      <Button variant="primary" type="signIn" onClick={onSubmit}>
-        로그인
-      </Button>
-      <Link to="/signup">
-        <Button variant="primary" type="signUp">
-          회원가입
-        </Button>
-      </Link>
-    </Form>
+    <Container>
+      <h1>SHRACK</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="signInId">
+          <FloatingLabel controlId="signInID" label="아이디" className="mb-3">
+            <Form.Control
+              type="ID"
+              placeholder="아이디"
+              onChange={e => setId(e.target.value)}
+            />
+          </FloatingLabel>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="signInPassword">
+          <FloatingLabel
+            controlId="signInPassword"
+            label="비밀번호"
+            className="mb-3">
+            <Form.Control
+              type="password"
+              placeholder="비밀번호"
+              onChange={e => setPassword(e.target.value)}
+            />
+          </FloatingLabel>
+        </Form.Group>
+        {isAlert && (
+          <Alert variant="warning" onClose={() => setIsAlert(false)}>
+            아이디와 비밀번호를 확인해주세요.
+          </Alert>
+        )}
+        <Row>
+          <Col className="d-flex justify-content-center align-items-center">
+            <Button
+              size="lg"
+              variant="primary"
+              type="signIn"
+              onClick={onSubmit}>
+              로그인
+            </Button>
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center">
+            <Link to="/signup">
+              <Button size="lg" variant="primary" type="signUp">
+                회원가입
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 }
 
