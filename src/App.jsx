7918a88c4616a7d8287941dc486842456f2527cd
Luchs41 <kokobo6@gmail.com>
2023-05-19 23:22:23 +0900

    App Routing 수정 : "/"를 Home으로 사용

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ function App() {
       <Route exact path="/" element={auth ? <Home /> : <Signin />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
