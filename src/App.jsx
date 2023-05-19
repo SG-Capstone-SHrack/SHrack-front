457b2a5f7618787ac1c9564d8e44bbeeda1225b1
Luchs41 <kokobo6@gmail.com>
2023-05-19 23:21:16 +0900

    App 수정

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <Routes>
-      <Route exact path="/" element={auth ? <Home /> : <Signin />} /> 
+      <Route exact path="/" element={auth ? <Home /> : <Signin />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/home" element={<Home />} />
