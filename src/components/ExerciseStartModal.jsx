a99fed807aca40f58c8296eabdac44e2c9a2a165
suminyu <suminei@naver.com>
2023-05-27 00:34:58 +0900

    운동 시작 모달 : 데이터 입력 칸 추가

--- src/components/ExerciseStartModal.jsx
+++ src/components/ExerciseStartModal.jsx
@@ -1,19 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Form, Modal } from 'react-bootstrap';
 
-import {
-  Button,
-  Form,
-  Container,
-  FloatingLabel,
-  Alert,
-  Modal,
-} from 'react-bootstrap';
-
-function ExerciseStartModal({isModalShow, setModalShow}) {
+function ExerciseStartModal({ isModalShow, setModalShow }) {
   const [exerciseType, setExerciseType] = useState('');
   const [exerciseGoal, setExerciseGoal] = useState('');
-  const [goalType, setGoalType] = useState('');
+  const history = useNavigate();
+
+  const handleModalSubmit = () => { //버튼 누르면 녹화 페이지로 데이터 넘기도록 => 추후 수정 예정
+    history.push('/녹화 페이지', { exerciseType, exerciseGoal });
+    setModalShow(false);
+  };
+
+  const yourExercisetypes = () => { //드롭다운 메뉴 구현
+    const exampleExerciseTypes = ['Push-up', 'Squat']; //운동 종류 임시 데이터 => 추후 수정 예정
+    return exampleExerciseTypes.map((type, index) => (
+      <option key={index} value={type}>
+        {type}
+      </option>
+    ));
+  };
+  const yourExercisegoals = () => {
+    const goals = Array.from({ length: 30 }, (_, index) => index + 1);  //운동 횟수 임시 데이터 => 추후 수정 예정
+    return goals.map((goal, index) => (
+      <option key={index} value={goal}>
+        {goal}
+      </option>
+    ));
+  };
+
   return (
     <div>
       <Modal
@@ -21,11 +36,42 @@ function ExerciseStartModal({isModalShow, setModalShow}) {
         onHide={() => setModalShow(false)}
         backdrop="static"
         keyboard={false}
-        centered>
+        centered
+      >
         <Modal.Header closeButton>
           <Modal.Title>운동 시작</Modal.Title>
         </Modal.Header>
-        <Modal.Body></Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="exerciseType">
+              <Form.Label>운동 종류 선택</Form.Label>
+              <Form.Control
+                as="select"
+                value={exerciseType}
+                onChange={(e) => setExerciseType(e.target.value)}
+              >
+                <option value="">종류를 선택하세요</option>
+                {yourExercisetypes()}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="exerciseGoal">
+              <Form.Label>운동 목표</Form.Label>
+              <Form.Control
+                as="select"
+                value={exerciseGoal}
+                onChange={(e) => setExerciseGoal(e.target.value)}
+              >
+                <option value="">횟수를 선택하세요</option>
+                {yourExercisegoals()}
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleModalSubmit}>
+            OK
+          </Button>
+        </Modal.Footer>
       </Modal>
     </div>
   );
