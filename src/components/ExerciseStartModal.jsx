2c0c26a4db09185776c88ed855623c3fc0198ef1
Luchs41 <kokobo6@gmail.com>
2023-05-28 20:58:05 +0900

    운동 시작 모달 수정 : 항목 추가

--- src/components/ExerciseStartModal.jsx
+++ src/components/ExerciseStartModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import {
@@ -7,36 +7,42 @@ import {
   Container,
   FloatingLabel,
   Alert,
+  Row,
+  Col,
   Modal,
 } from 'react-bootstrap';
 
 function ExerciseStartModal({ isModalShow, setModalShow }) {
   const [exerciseType, setExerciseType] = useState('');
-  const [exerciseGoal, setExerciseGoal] = useState('');
-  const history = useNavigate();
+  const [exerciseGoal, setExerciseGoal] = useState(0);
+  const [exerciseMass, setExerciseMass] = useState(0);
+  useEffect(() => {
+    // execiseType and execiseGoal console.log
+    console.log(exerciseType);
+    console.log(exerciseGoal);
+  }, [exerciseType, exerciseGoal]);
 
   const handleModalSubmit = () => {
-    //버튼 누르면 녹화 페이지로 데이터 넘기도록 => 추후 수정 예정
-    history.push('/녹화 페이지', { exerciseType, exerciseGoal });
     setModalShow(false);
   };
 
   const yourExercisetypes = () => {
     //드롭다운 메뉴 구현
-    const exampleExerciseTypes = ['Push-up', 'Squat']; //운동 종류 임시 데이터 => 추후 수정 예정
-    return exampleExerciseTypes.map((type, index) => (
-      <option key={index} value={type}>
-        {type}
-      </option>
-    ));
-  };
-  const yourExercisegoals = () => {
-    const goals = Array.from({ length: 30 }, (_, index) => index + 1); //운동 횟수 임시 데이터 => 추후 수정 예정
-    return goals.map((goal, index) => (
-      <option key={index} value={goal}>
-        {goal}
-      </option>
-    ));
+    const exampleExerciseTypes = ['pushup', 'squat'];
+    return exampleExerciseTypes.map((type, index) => {
+      let message = '';
+      if (type === 'pushup') {
+        message = '팔굽혀펴기';
+      } else if (type === 'squat') {
+        message = '스쿼트';
+      }
+
+      return (
+        <option key={index} value={type}>
+          {message}
+        </option>
+      );
+    });
   };
 
   return (
@@ -52,26 +58,59 @@ function ExerciseStartModal({ isModalShow, setModalShow }) {
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group controlId="exerciseType">
-              <Form.Label>운동 종류 선택</Form.Label>
-              <Form.Control
-                as="select"
-                value={exerciseType}
-                onChange={e => setExerciseType(e.target.value)}>
-                <option value="">종류를 선택하세요</option>
-                {yourExercisetypes()}
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="exerciseGoal">
-              <Form.Label>운동 목표</Form.Label>
-              <Form.Control
-                as="select"
-                value={exerciseGoal}
-                onChange={e => setExerciseGoal(e.target.value)}>
-                <option value="">횟수를 선택하세요</option>
-                {yourExercisegoals()}
-              </Form.Control>
-            </Form.Group>
+            <Row>
+              <Col>
+                <Form.Group controlId="exerciseType">
+                  <Form.Label>운동 종류 선택</Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={exerciseType}
+                    onChange={e => setExerciseType(e.target.value)}>
+                    <option value="">종류를 선택하세요</option>
+                    {yourExercisetypes()}
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="exerciseMass">
+                  <Form.Label>운동 무게(kg)</Form.Label>
+                  <Form.Control
+                    as="input"
+                    type="number"
+                    value={exerciseMass}
+                    onChange={e =>
+                      setExerciseMass(e.target.value)
+                    }></Form.Control>
+                </Form.Group>
+              </Col>
+            </Row>
+            <br />
+            <Row>
+              <Col>
+                <Form.Group controlId="exerciseGoal">
+                  <Form.Label>운동 목표(횟수)</Form.Label>
+                  <Form.Control
+                    as="input"
+                    type="number"
+                    value={exerciseGoal}
+                    onChange={e =>
+                      setExerciseGoal(e.target.value)
+                    }></Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="exerciseMass">
+                  <Form.Label>운동 무게(kg)</Form.Label>
+                  <Form.Control
+                    as="input"
+                    type="number"
+                    value={exerciseMass}
+                    onChange={e =>
+                      setExerciseMass(e.target.value)
+                    }></Form.Control>
+                </Form.Group>
+              </Col>
+            </Row>
           </Form>
         </Modal.Body>
         <Modal.Footer>
