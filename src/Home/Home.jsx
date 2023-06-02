03559d4fe2322b999713186b9ce7a2a11e8013f2
suminyu <suminei@naver.com>
2023-06-02 12:45:00 +0900

    HOME 수정 : 주석 추가

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,67 +5,64 @@ import 'react-datepicker/dist/react-datepicker.css';
 import { Card, Navbar, Nav, Button, ProgressBar } from 'react-bootstrap';
 import axios from 'axios';
 import ExerciseStartModal from '../components/ExerciseStartModal';
-import { formatISO } from 'date-fns';
+import { formatISO } from 'date-fns';  //날짜 포매팅 위해 추가
 
 const HomePage = () => {
-  const [id, setId] = useState('');
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [id, setId] = useState('');  //id 상수 추가
+  const [selectedDate, setSelectedDate] = useState(new Date()); //날짜 상수 추가
+
   const [exerciseRecords, setExerciseRecords] = useState([]);
   const [showModal, setShowModal] = useState(false);
-  const handleLogout = () => {
+  const handleLogout = () => { //로그아웃 시 : 로컬스토리지에서 auth 삭제 후 홈으로 이동
     localStorage.removeItem('auth');
     window.location.href = '/';
   };
-  useEffect(() => {
+  useEffect(() => { //로그인 시 : 로컬스토리지에서 auth 가져와서 저장
     const authId = localStorage.getItem('auth');
     setId(authId);
   }, []);
 
-  const handleDateChange = (date) => {
+  const handleDateChange = (date) => { //date-picker에서 날짜 선택할 때마다 선택한 날짜로 상수 변경 후 저장
     setSelectedDate(date);
   };
-  const sendExerciseData = async () => {
+  useEffect(() => { //날짜 선택할 때마다 데이터 보내기 위해 끝부분에 [selectedDate] 추가
+    sendExerciseData();
+  }, [selectedDate]);
+
+  const sendExerciseData = async () => { //서버에 데이터 보내기(id, date 정보)
     try {
       const formattedDate = formatISO(selectedDate, { representation: 'date' });
       const exerciseData = {
         id,
         date: formattedDate,
       };
-      const response = await axios.post(
+      const response = await axios.post( 
+      //서버에서 id, date정보에 매칭하는 데이터 받아오기 => 서버로 넘길 때 CORS 에러 발생하는 것 때문에 allow cors 설치하고 켜서 테스트 진행했습니다
         'http://13.209.109.234:5000/exercise_log',
         exerciseData,
         {
           headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json', //json 형식으로 받아오는 부분
           },
         }
       );
-
-      setExerciseRecords(response.data.exercise_log);
+      setExerciseRecords(response.data.exercise_log); //다시 exercise_log에 저장
     } catch (error) {
       console.log(error);
     }
   };
 
-  const handleAddExercise = () => {
+  const handleAddExercise = () => {  //모달창 띄우고 닫는 부분
     setShowModal(true);
   };
-
   const handleModalClose = () => {
     setShowModal(false);
   };
-
   const handleModalSubmit = () => {
     setShowModal(false);
   };
 
-
-
-  useEffect(() => {
-    sendExerciseData();
-  }, [selectedDate]);
-
-  return (
+  return ( //상단 메뉴바
     <div>
       <Navbar bg="primary" variant="dark" expand="lg">
         <Navbar.Brand>SHrack</Navbar.Brand>
@@ -91,7 +88,7 @@ const HomePage = () => {
         <div style={{ width: '500px' }}>
           <Card.Body>
             <Card.Title style={{ marginBottom: '20px' }}>
-              Welcome to SHrack! Hello {id ? `User ${id}` : ''}
+              Welcome to SHrack! Hello {id ? `User ${id}` : ''} 
             </Card.Title>
             <DatePicker
               selected={selectedDate}
