6d6be29ee1bbbcd92cb8a3dbacba1f126915c4bb
Luchs41 <kokobo6@gmail.com>
2023-06-05 15:04:39 +0900

    운동 기록(카메라) : 구조 수정

--- src/record/Camera.jsx
+++ src/record/Camera.jsx
@@ -1,18 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
-import {
-  Card,
-  Button,
-  Form,
-  Container,
-  FloatingLabel,
-  Alert,
-  Row,
-  Col,
-  Modal,
-} from 'react-bootstrap';
+import { Card, Button, Alert } from 'react-bootstrap';
 import styles from './Camera.module.css';
 import axios from 'axios';
-import { clear } from '@testing-library/user-event/dist/clear';
 
 function CameraComponent({
   count,
@@ -27,14 +16,12 @@ function CameraComponent({
   startTime,
 }) {
   const videoRef = useRef(null);
-  //const [hasPermission, setHasPermission] = useState(null);
   const [stream, setStream] = useState(null);
-  //사용자 디바이스에 카메라가 없다면, 카메라가 없다는 메시지를 보여줌
+  const [intervalId, setIntervalId] = useState(null);
 
   const [userId, setUserId] = useState('');
   const [uuid, setUuid] = useState('');
 
-  // exercise_type은 exerciseType과 exerciseDirection을 합친 값
   let exercise_type = exerciseType + '-' + exerciseDirection;
   if (exerciseType === 'squat') {
     exercise_type = exercise_type + '-leg';
@@ -42,10 +29,47 @@ function CameraComponent({
     exercise_type = exercise_type + '-arm';
   }
 
-  // 사용자가 카메라 접근 허용을 했는지 확인
-  // 만약 허용을 안했다면, 허용을 요청
+  const sendImage = () => {
+    if (!hasPermission) {
+      return;
+    }
+    const canvas = document.createElement('canvas');
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+    canvas.getContext('2d').drawImage(videoRef.current, 0, 0);
+
+    canvas.toBlob(
+      blob => {
+        const formData = new FormData();
+        formData.append('file', blob, 'file.jpg');
+        console.log(formData);
+
+        axios
+          .post(
+            `https://shrack-ml.p-e.kr/inference/image/${userId}/${uuid}/${exercise_type}`,
+            formData,
+            {
+              headers: {
+                'Content-Type': 'multipart/form-data',
+              },
+            },
+          )
+          .then(res => {
+            console.log(res.data.count);
+            if (parseInt(res.data.count) > count) {
+              setCount(res.data.count);
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      },
+      'image/jpeg',
+      0.1,
+    );
+  };
+
   useEffect(() => {
-    // uuid 설정 -> 6자리 난수
     const uuid = Math.floor(Math.random() * 1000000);
     setUuid(uuid);
 
@@ -60,23 +84,43 @@ function CameraComponent({
         setHasPermission(false);
         console.log(err);
       });
-  }, []); // 이거 괄호 뺴면 안됨
-  // 카메라 접근 허용을 했다면, videoRef에 stream을 넣어줌
+
+    return () => {
+      clearInterval(intervalId); // Clean up interval on unmount
+    };
+  }, []);
+
   useEffect(() => {
     if (videoRef.current && stream) {
       videoRef.current.srcObject = stream;
     }
   }, [hasPermission, stream]);
 
-  // 카메라가 없다면, 카메라가 없다는 메시지를 보여줌
+  useEffect(() => {
+    if (isExerciseEnd) {
+      clearInterval(intervalId); // Clear the interval when exercise ends
+    }
+  }, [isExerciseEnd]);
+
+  useEffect(() => {
+    if (hasPermission && !isExerciseEnd) {
+      const newIntervalId = setInterval(sendImage, 5000);
+      setIntervalId(newIntervalId);
+
+      return () => {
+        clearInterval(newIntervalId); // Clean up interval on unmount
+      };
+    }
+  }, [hasPermission, isExerciseEnd]);
+
   if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
     return <Alert variant="danger">기기에 카메라가 없습니다.</Alert>;
   }
-  // 카메라 접근 허용을 했다면, videoRef를 보여줌
+
   if (hasPermission === null) {
     return <div />;
   }
-  // 카메라 접근 허용을 하지 않았다면, 허용을 요청하는 버튼 보여줌
+
   if (hasPermission === false) {
     return (
       <Card className="text-center" style={{ width: '80%', margin: '1rem' }}>
@@ -103,54 +147,6 @@ function CameraComponent({
         </Button>
       </Card>
     );
-  }
-  const sendImage = () => {
-    if (!hasPermission) {
-      return;
-    }
-    const canvas = document.createElement('canvas');
-    canvas.width = videoRef.current.videoWidth;
-    canvas.height = videoRef.current.videoHeight;
-    canvas.getContext('2d').drawImage(videoRef.current, 0, 0);
-
-    canvas.toBlob(
-      blob => {
-        const formData = new FormData();
-        formData.append('file', blob, 'file.jpg');
-        console.log(formData);
-        //http://34.69.53.183:8090
-        axios
-          .post(
-            `https://shrack-ml.p-e.kr/inference/image/${userId}/${uuid}/${exercise_type}`,
-            formData,
-            {
-              headers: {
-                'Content-Type': 'multipart/form-data',
-              },
-            },
-          )
-          .then(res => {
-            console.log(res.data.count);
-            if (parseInt(res.data.count) > count) {
-              setCount(res.data.count);
-            }
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      },
-      'image/jpeg',
-      0.1,
-    );
-  };
-  const repeatWork = () => {
-    sendImage();
-    setTimeout(repeatWork, 5000);
-  };
-  const timeoutId = setTimeout(repeatWork, 5000);
-  //clearTimeout(timeoutId); //testpurpose
-  if (isExerciseEnd) {
-    clearTimeout(timeoutId);
   }
 
   return (
